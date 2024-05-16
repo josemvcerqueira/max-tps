6e8901f9d2eb366899924e876de85a1bfdf6a92a
Jose Cerqueira <josecerqueira@protonmail.com>
2024-05-16 18:59:33 +0100

    :zap: feat: even faster

--- script/src/index.ts
+++ script/src/index.ts
@@ -7,26 +7,29 @@ import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
 import { SuiObjectRef } from '@mysten/sui.js/dist/cjs/transactions';
 import util from 'util';
+import invariant from 'tiny-invariant';
 
 export const log = (x: unknown) =>
   console.log(util.inspect(x, false, null, true));
 
 // TODO SET THESE. Increase the budget and price if needed.
-const PRIVATE_KEY = '';
+// TODO ADD same number of keys and RPCs, we might need diff URLs. Try with the same one first
+const PRIVATE_KEYS = [''];
+const RPC_URLS = [getFullnodeUrl('testnet')];
 const SEND_TRANSACTION_GAS_BUDGET = 30_000_000n;
 const GET_GAS_COINS_GAS_BUDGET = 20_000_000n;
 const GAS_PRICE = 1100n;
 const TPS = 10;
 const SEQUENT_TXS = 10n;
-const USELESS_PKG = '';
+const USELESS_PKG =
+  '0xcd7af24572133a6772fae2867d37dd65f817da917cb44a056ec38743211f66cc';
 
 const AMOUNTS = Array(TPS).fill(SEND_TRANSACTION_GAS_BUDGET * SEQUENT_TXS);
 
-const suiClient = new SuiClient({ url: getFullnodeUrl('testnet') });
-
-const keyPair = Ed25519Keypair.fromSecretKey(
-  Uint8Array.from(Buffer.from(PRIVATE_KEY, 'base64')).slice(1)
-);
+const makeKeyPair = (key: string) =>
+  Ed25519Keypair.fromSecretKey(
+    Uint8Array.from(Buffer.from(key, 'base64')).slice(1)
+  );
 
 interface SendTransactionArgs {
   suiClient: SuiClient;
@@ -98,7 +101,13 @@ const sendTransaction = async ({
   return result.effects?.mutated!;
 };
 
-const loop = async (data: OwnedObjectRef[][]) => {
+interface LoopArgs {
+  data: OwnedObjectRef[][];
+  suiClient: SuiClient;
+  keyPair: Ed25519Keypair;
+}
+
+const loop = async ({ data, suiClient, keyPair }: LoopArgs) => {
   const promises = [];
   for (const result of data) {
     promises.push(
@@ -114,12 +123,19 @@ const loop = async (data: OwnedObjectRef[][]) => {
 
   log('Batch sent');
 
-  if (remainingGasCoin.length) await loop(remainingGasCoin);
+  if (remainingGasCoin.length)
+    await loop({ data: remainingGasCoin, suiClient, keyPair });
 };
 
-const main = async () => {
+interface RunArgs {
+  suiClient: SuiClient;
+  keyPair: Ed25519Keypair;
+}
+
+const run = async ({ suiClient, keyPair }: RunArgs) => {
   try {
     const gasCoins = await getGasCoins({ suiClient, keyPair });
+    invariant(gasCoins.length, 'Failed to create gas coins');
 
     log('Gas Coins Created');
 
@@ -134,12 +150,26 @@ const main = async () => {
     const remainingGasCoin = await Promise.all(promises);
     log('Batch sent');
 
-    if (remainingGasCoin?.length) await loop(remainingGasCoin);
+    if (remainingGasCoin?.length)
+      await loop({ data: remainingGasCoin, suiClient, keyPair });
 
     log('Script ran!');
   } catch (e) {
     log(e);
   }
+};
+
+const main = () => {
+  invariant(PRIVATE_KEYS.length, 'Missing private keys');
+  invariant(RPC_URLS.length, 'Missing RPCs');
+  invariant(PRIVATE_KEYS.length === RPC_URLS.length, 'Wrong set up');
+
+  PRIVATE_KEYS.forEach((key, index) => {
+    run({
+      suiClient: new SuiClient({ url: RPC_URLS[index] }),
+      keyPair: makeKeyPair(key),
+    });
+  });
 };
 
 main();
